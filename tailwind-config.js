008ffab8691b0a9fc98b266df32dc84b29d4be05
GSU-SAIL-Lab <sail@georgiasouthern.edu>
2026-05-28 10:03:26 -0700

    Add files via upload

---
+++ tailwind-config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                gsuNavy: "#041E42" /* True Georgia Southern Navy */,
+                gsuGold: "#A89968" /* True Georgia Southern Gold */,
+            },
+            fontFamily: {
+                sans: ["Inter", "system-ui", "sans-serif"],
+            },
+            width: {
+                '9/10': '90%',
+                '7/10': '70%',
+            }
+        },
+    },
+};
